db0170f105c59849d05a81d8dfd290daa0608e65
Natalia <patatosik@gmail.com>
2018-06-30 14:37:53 -0500

    Добавляет анимацию ошибки

--- js/script.js
+++ js/script.js
@@ -31,11 +31,15 @@ feedbackButton.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error")
 });
 
 form.addEventListener("submit", function(evt) {
   if (!nameField.value || !emailField.value) {
     evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
   } else {
     if (isStorageSupport) {
       localStorage.setItem("name", nameField.value);
@@ -49,6 +53,7 @@ window.addEventListener("keydown", function (evt) {
     evt.preventDefault();
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error")
     }
 }
 });
